135502a1e24339749f59e4b37cebb5a6cd5b473f
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-07 13:58:16 +0300

    Exercise 7:
    To register SchoolContext as a service, open Startup.cs, and add the highlighted lines to the ConfigureServices method.

---
+++ CSHTML09.07.23/Startup.cs
@@ -0,0 +1,24 @@
+﻿using CSHTML09._07._23.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace CSHTML09._07._23
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllersWithViews();
+        }
+    }    }
