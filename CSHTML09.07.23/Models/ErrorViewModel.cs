714ddc0b4d813a0e7529eaf378f53c710d53594b
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-07 11:57:15 +0300

    Add project files.

---
+++ CSHTML09.07.23/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSHTML09._07._23.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
