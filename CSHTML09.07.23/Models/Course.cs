b44dfe21efb60217fa3a1ef7ce747740ab2ce1a7
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-07 13:18:28 +0300

    Exercise 4:
    create 3 classes in folder models one-to-many relationship with.

--- CSHTML09.07.23/Models/Course.cs
+++ CSHTML09.07.23/Models/Course.cs
@@ -1,6 +1,14 @@
-﻿namespace CSHTML09._07._23
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CSHTML09._07._23.Models
 {
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; }
     }
 }
--- CSHTML09.07.23/Models/Enrollment.cs
+++ CSHTML09.07.23/Models/Enrollment.cs
@@ -1,6 +1,20 @@
-﻿namespace CSHTML09._07._23
+﻿namespace CSHTML09._07._23.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
     public class Enrollment
     {
+
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        // Navigation properties to represent the relationships with Student and Course
+        public Student Student { get; set; }
+        public Course Course { get; set; }
     }
 }
