5cd17a8a37290a1d9d990e0dc6a6ff86820bac32
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-17 14:46:36 +0300

    Exercise 24:
    1.Create a SchoolViewModels folder in the Models folder.
    2. add a class file EnrollmentDateGroup.cs

---
+++ CSHTML09.07.23/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSHTML09._07._23.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
