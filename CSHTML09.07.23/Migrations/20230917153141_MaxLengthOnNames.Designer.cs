12486e720ffb22c23a4a684a5db5fac8a074e3a2
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-17 18:34:04 +0300

    Exercise 26:
    1.In Models/Student.cs, add a using statement.
    2. add DataType
    3. ensure that users don't enter more than 50 characters for a name.
    4. Add-Migration MaxLengthOnNames
    5.Update-Database

--- CSHTML09.07.23/Migrations/20230917153141_MaxLengthOnNames.Designer.cs
+++ CSHTML09.07.23/Migrations/20230917153141_MaxLengthOnNames.Designer.cs
@@ -4,6 +4,7 @@ using CSHTML09._07._23.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CSHTML09._07._23.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20230917153141_MaxLengthOnNames")]
+    partial class MaxLengthOnNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CSHTML09.07.23/Models/Student.cs
+++ CSHTML09.07.23/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
 
 namespace CSHTML09._07._23.Models
 {
@@ -7,11 +9,24 @@ namespace CSHTML09._07._23.Models
     {
         [Key]
         public int  ID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        //The DisplayFormat attribute is used to explicitly specify the date format
+        //The ApplyFormatInEditMode setting specifies that the formatting should also be
+        //applied when the value is displayed in a text box for editing
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
         public DateTime EnrollmentDate { get; set; }
 
         // Navigation property to represent the relationship with Enrollments
         public ICollection<Enrollment> Enrollments { get; }
     }
 }
+
+
+////the following code requires the first character to be upper case and the remaining characters to be alphabetical
+///[RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
