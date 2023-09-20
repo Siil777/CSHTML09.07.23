1ce84c66fb3c109e1aace95fff4ddadbbae60211
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-20 13:37:59 +0300

    Exercise 27:
    1.Create in models Instructors.cs
    2. Create in models OfficeAssignment.cs
    3. Modify Course entity

--- CSHTML09.07.23/Models/Course.cs
+++ CSHTML09.07.23/Models/Course.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace CSHTML09._07._23.Models
@@ -6,10 +7,19 @@ namespace CSHTML09._07._23.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+        [Range(0, 5)]
         public int Credits { get; set; }
 
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+
         public ICollection<Enrollment> Enrollments { get; set; }
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
---
+++ CSHTML09.07.23/Models/OfficeAssignment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSHTML09._07._23.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        public int InstructorID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public Instructor Instructor { get; set; }
+    }
+}
